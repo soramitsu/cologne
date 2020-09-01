94c8797e880d48803010bec0a0e660e11058968a
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2020-09-01 10:28:27 +0300

    tests rearranged
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- src/test/kotlin/VaultTest.kt
+++ src/test/kotlin/VaultTest.kt
@@ -8,7 +8,6 @@ import org.testcontainers.containers.GenericContainer
 import org.testcontainers.images.builder.ImageFromDockerfile
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.web3j.protocol.core.DefaultBlockParameter
 import org.web3j.protocol.exceptions.TransactionException
 import java.math.BigInteger
 import java.nio.file.Path
@@ -79,61 +78,6 @@ class VaultTest {
     }
 
     /**
-     * @given MedleyDAO deployed owner can borrow 50 EAU
-     * @when the owner borrows 50 EAU
-     * @then EAU tokens are minted to the owner account, owner debt is 50 EAU
-     */
-    @Test
-    fun borrowSunnyDay() {
-        ownerCreatesVault(initialAmount, tokenPrice)
-        val initialEauSupply = helper.eauToken.totalSupply().send()
-        val initialOwnerEauBalance = helper.eauToken.balanceOf(owner).send()
-        val toBorrow = vault.creditLimit.send()
-
-        val tx = vault.borrow(toBorrow).send()
-
-        assertEquals(initialEauSupply.plus(toBorrow), helper.eauToken.totalSupply().send())
-        assertEquals(initialOwnerEauBalance.plus(toBorrow), helper.eauToken.balanceOf(owner).send())
-        assertEquals(toBorrow, vault.principal.send())
-        val timestamp =
-            helper.web3.ethGetBlockByNumber(DefaultBlockParameter.valueOf(tx.blockNumber), false).send().block.timestamp
-        assertEquals(toBorrow, vault.getTotalDebt(timestamp).send())
-    }
-
-    /**
-     * @given a vault is deployed by the owner
-     * @when a stranger account sends borrow
-     * @then error returned - operation is restricted to the owner only
-     */
-    @Test
-    fun borrowBy3rdParty() {
-        ownerCreatesVault(initialAmount, tokenPrice)
-        val stranger = helper.credentialsBob
-        val strangerVault = Vault.load(vault.contractAddress, helper.web3, stranger, helper.gasProvider)
-        val toBorrow = BigInteger.TEN
-
-        assertThrows<TransactionException> {
-            strangerVault.borrow(toBorrow).send()
-        }
-    }
-
-    /**
-     * @given MedleyDAO deployed owner has borrowed all limit
-     * @when the owner borrows 50 EAU more
-     * @then Error returned - credit limit exhausted
-     */
-    @Test
-    fun borrowExceedsLimit() {
-        ownerCreatesVault(initialAmount, tokenPrice)
-        val toBorrow = vault.creditLimit.send()
-        vault.borrow(toBorrow).send()
-
-        assertThrows<TransactionException> {
-            vault.borrow(toBorrow).send()
-        }
-    }
-
-    /**
      * @given owner borrowed 100'000 EAU
      * @when calculate total debt 1 year later called
      * @then 110276 EAU returned (10276 EAU accrued as interest)
@@ -152,32 +96,6 @@ class VaultTest {
         val oneYearLater = timestamp.plus(BigInteger.valueOf(31536000));
         helper.timeProvider.setTime(timestamp)
         assertEquals(BigInteger.valueOf(110276), vault.getTotalDebt(oneYearLater).send())
-    }
-
-    /**
-     * @given Vault is created and owner debt is 100'000 EAU and 0 fees accrued
-     * @when the owner pays off 50'000 EAU
-     * @then Debt is reduced to 50'000 EAU and 50'000 EAU are burnt
-     */
-    @Test
-    fun payOffDebtPartially() {
-        val initialAmount = BigInteger.valueOf(500_000)
-        val tokenPrice = BigInteger.valueOf(4)
-        ownerCreatesVault(initialAmount, tokenPrice)
-        val debtBefore = BigInteger.valueOf(100_000)
-        vault.borrow(debtBefore).send()
-        val timestamp = helper.timeProvider.time.send()
-        val toPayOff = BigInteger.valueOf(50_000)
-        val initialEauSupply = eauToken.totalSupply().send()
-        val balanceBefore = eauToken.balanceOf(owner).send()
-
-        eauToken.approve(vault.contractAddress, toPayOff).send()
-        vault.payOff(toPayOff).send()
-
-        val newDebt = vault.getTotalDebt(timestamp).send()
-        assertEquals(debtBefore.minus(toPayOff), newDebt)
-        assertEquals(initialEauSupply.minus(toPayOff), eauToken.totalSupply().send())
-        assertEquals(balanceBefore.minus(toPayOff), eauToken.balanceOf(owner).send())
     }
 
     /**
@@ -234,8 +152,8 @@ class VaultTest {
     }
 
     /**
-     * @given a vault with enough user token and a buyer with 40 EAU and price is 2 EAU/TKN
-     * @when the buyer buys 20 TKN with price at least 2 EAU/TKN
+     * @given a vault with enough user token and a buyer with 40 EAU and price is 2 TKN/EAU
+     * @when the buyer buys 20 TKN with price at least 2 TKN/EAU
      * @then 40 EAU go to the vault and 20 TKN go to the buyer
      */
     @Test
@@ -255,5 +173,157 @@ class VaultTest {
         assertEquals(BigInteger.ZERO, eauToken.balanceOf(buyer.address).send())
         assertEquals(initialAmount.minus(toBuy), userToken.balanceOf(vault.contractAddress).send())
         assertEquals(toBuy, userToken.balanceOf(buyer.address).send())
+    }
+
+    /**
+     * @given a vault has insufficient user token (1 TKN) and a buyer with 40 EAU and price is 2 TKN/EAU
+     * @when the buyer buys 20 TKN with price at least 2 TKN/EAU
+     * @then deal rejected, balances are not changed
+     */
+    @Test
+    fun buyTokensNotEnoughTokens() {
+        val insufficientAmount = BigInteger.ONE
+        ownerCreatesVault(insufficientAmount, tokenPrice)
+        val buyer = helper.credentialsBob
+        val buyerVault = Vault.load(vault.contractAddress, helper.web3, buyer, helper.gasProvider)
+        val toBuy = BigInteger.valueOf(20)
+        val costInEau = toBuy.multiply(tokenPrice)
+        eauToken.mint(buyer.address, costInEau).send()
+        val buyerEAUToken = EAUToken.load(eauToken.contractAddress, helper.web3, buyer, helper.gasProvider)
+
+        buyerEAUToken.approve(vault.contractAddress, costInEau).send()
+        assertThrows<TransactionException> {
+            buyerVault.buy(toBuy, tokenPrice, buyer.address).send()
+        }
+
+        assertEquals(BigInteger.ZERO, eauToken.balanceOf(vault.contractAddress).send())
+        assertEquals(costInEau, eauToken.balanceOf(buyer.address).send())
+        assertEquals(insufficientAmount, userToken.balanceOf(vault.contractAddress).send())
+        assertEquals(BigInteger.ZERO, userToken.balanceOf(buyer.address).send())
+    }
+
+    /**
+     * @given a vault has enough user token and a buyer with 40 EAU and price is 2 TKN/EAU
+     * @when the buyer buys 20 TKN with price at least 1 TKN/EAU
+     * @then deal rejected, balances are not changed
+     */
+    @Test
+    fun buyTokensPriceTooLow() {
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val buyer = helper.credentialsBob
+        val buyerVault = Vault.load(vault.contractAddress, helper.web3, buyer, helper.gasProvider)
+        val toBuy = BigInteger.valueOf(20)
+        val priceTooHigh = BigInteger.ONE
+        val costInEau = toBuy.multiply(priceTooHigh)
+        eauToken.mint(buyer.address, costInEau).send()
+        val buyerEAUToken = EAUToken.load(eauToken.contractAddress, helper.web3, buyer, helper.gasProvider)
+
+        buyerEAUToken.approve(vault.contractAddress, costInEau).send()
+        assertThrows<TransactionException> {
+            buyerVault.buy(toBuy, priceTooHigh, buyer.address).send()
+        }
+
+        assertEquals(BigInteger.ZERO, eauToken.balanceOf(vault.contractAddress).send())
+        assertEquals(costInEau, eauToken.balanceOf(buyer.address).send())
+        assertEquals(initialAmount, userToken.balanceOf(vault.contractAddress).send())
+        assertEquals(BigInteger.ZERO, userToken.balanceOf(buyer.address).send())
+    }
+
+    /**
+     * @given MedleyDAO deployed owner can borrow 50 EAU
+     * @when the owner borrows 50 EAU
+     * @then EAU tokens are minted to the owner account, owner debt is 50 EAU
+     */
+    @Test
+    fun borrowSunnyDay() {
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val initialEauSupply = helper.eauToken.totalSupply().send()
+        val initialOwnerEauBalance = helper.eauToken.balanceOf(owner).send()
+        val toBorrow = vault.creditLimit.send()
+
+        vault.borrow(toBorrow).send()
+
+        assertEquals(initialEauSupply.plus(toBorrow), helper.eauToken.totalSupply().send())
+        assertEquals(initialOwnerEauBalance.plus(toBorrow), helper.eauToken.balanceOf(owner).send())
+        assertEquals(toBorrow, vault.principal.send())
+        val timestamp = helper.timeProvider.time.send()
+        assertEquals(toBorrow, vault.getTotalDebt(timestamp).send())
+    }
+
+    /**
+     * @given a vault is deployed by the owner
+     * @when a stranger account sends borrow
+     * @then error returned - operation is restricted to the owner only
+     */
+    @Test
+    fun borrowBy3rdParty() {
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val stranger = helper.credentialsBob
+        val strangerVault = Vault.load(vault.contractAddress, helper.web3, stranger, helper.gasProvider)
+        val toBorrow = BigInteger.TEN
+
+        assertThrows<TransactionException> {
+            strangerVault.borrow(toBorrow).send()
+        }
+    }
+
+    /**
+     * @given MedleyDAO deployed owner has borrowed all limit
+     * @when the owner borrows 50 EAU more
+     * @then Error returned - credit limit exhausted
+     */
+    @Test
+    fun borrowExceedsLimit() {
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val toBorrow = vault.creditLimit.send()
+        vault.borrow(toBorrow).send()
+
+        assertThrows<TransactionException> {
+            vault.borrow(toBorrow).send()
+        }
+    }
+
+    /**
+     * @given The vault deployed with no debt
+     * @when the user pays off X EAU
+     * @then the vault balance increased by X EAU
+     */
+    @Test
+    fun payOffNoDebt() {
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val toPayOff = BigInteger.valueOf(50_000)
+        helper.eauToken.mint(owner, toPayOff).send()
+
+        eauToken.approve(vault.contractAddress, toPayOff).send()
+        vault.payOff(toPayOff).send()
+
+        assertEquals(toPayOff, eauToken.balanceOf(vault.contractAddress).send())
+        assertEquals(BigInteger.ZERO, eauToken.balanceOf(owner).send())
+    }
+
+    /**
+     * @given Vault is created and owner debt is 100'000 EAU and 0 fees accrued
+     * @when the owner pays off 50'000 EAU
+     * @then Debt is reduced to 50'000 EAU and 50'000 EAU are burnt
+     */
+    @Test
+    fun payOffDebtPartially() {
+        val initialAmount = BigInteger.valueOf(500_000)
+        val tokenPrice = BigInteger.valueOf(4)
+        ownerCreatesVault(initialAmount, tokenPrice)
+        val debtBefore = BigInteger.valueOf(100_000)
+        vault.borrow(debtBefore).send()
+        val timestamp = helper.timeProvider.time.send()
+        val toPayOff = BigInteger.valueOf(50_000)
+        val initialEauSupply = eauToken.totalSupply().send()
+        val balanceBefore = eauToken.balanceOf(owner).send()
+
+        eauToken.approve(vault.contractAddress, toPayOff).send()
+        vault.payOff(toPayOff).send()
+
+        val newDebt = vault.getTotalDebt(timestamp).send()
+        assertEquals(debtBefore.minus(toPayOff), newDebt)
+        assertEquals(initialEauSupply.minus(toPayOff), eauToken.totalSupply().send())
+        assertEquals(balanceBefore.minus(toPayOff), eauToken.balanceOf(owner).send())
     }
 }
